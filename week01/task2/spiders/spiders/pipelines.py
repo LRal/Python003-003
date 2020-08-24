ef6e00745e647a73420e0f108525d94c2a37adad
unknown <04895@BTRCHINA>
2020-08-24 16:42:51 +0800

    一些代码上的修改

---
+++ week01/task2/spiders/spiders/pipelines.py
@@ -0,0 +1,20 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class SpidersPipeline:
+    def process_item(self, item, spider):
+        title = item['title']
+        genre = item['genre']
+        release_date = item['release_date']
+        output = f'|{title}| |{genre}| |{release_date}|\n'
+
+        with open('./movie_info.csv', 'a+', encoding='gbk') as article:
+            article.write(output)
+        return item
